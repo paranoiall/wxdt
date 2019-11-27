1ccd40faa7eebc38504cf08346b731c5b049a1a6
paranoiall <1803230386@qq.com>
2019-11-27 17:45:30 +0800

    laravel+vue成功

--- vxapi/resources/views/index.blade.php
+++ vxapi/resources/views/index.blade.php
@@ -1,19 +1,12 @@
-<!Doctype html>
-<html lang="{{ app()->getLocale() }}">
+<!doctype html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Laravel</title>
-
+    <title>Larvue</title>
 </head>
 <body>
-<div id="app">
-    <example-component></example-component>
-</div>
-asdf
-</body>
-
+<div id="app"></div>
 <script src="{{ mix('js/app.js') }}"></script>
+</body>
 </html>
